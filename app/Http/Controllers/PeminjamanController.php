f46706dfb5b00d0759394d8177f06c868c8e76c9
Adrian Maulani <maulani.ad@gmail.com>
2024-06-27 03:51:02 +0700

    fix(peminjaman): logic stok addition and deduction

--- app/Http/Controllers/PeminjamanController.php
+++ app/Http/Controllers/PeminjamanController.php
@@ -14,8 +14,8 @@ class PeminjamanController extends Controller
 {
     public function index(Request $request): View
     {
-        $listBuku = Buku::all()->sortByDesc('created_at');
-        $listUser = User::all()->sortByDesc('created_at');
+        $listBuku = Buku::all()->sortBy('judul')->values()->all();
+        $listUser = User::all()->sortBy('name')->values()->all();
         $listPeminjaman = Peminjaman::with(['buku', 'user'])->get();
 
         return view('peminjaman', [
@@ -29,6 +29,14 @@ class PeminjamanController extends Controller
     {
         $data = $request->post();
 
+        $buku = Buku::find($data['id_buku']);
+        if ($buku->stok < $data['jumlah_pinjaman']) {
+            return redirect()->route('peminjaman.index')->with(['status' => 'ERROR', 'message' => 'Stok buku tidak mencukupi']);
+        }
+
+        $buku->stok = $buku->stok - $data['jumlah_pinjaman'];
+        $buku->save();
+
         Peminjaman::create($data);
 
         return redirect()->route('peminjaman.index')->with('status', "SUCCESS");
@@ -36,14 +44,56 @@ class PeminjamanController extends Controller
 
     public function update(Request $request, $id): RedirectResponse
     {
-        $data = $request->except('_token');
-        Peminjaman::where('id_peminjaman', $id)->update($data);
+        $data = $request->validate([
+            'id_buku' => ['sometimes', 'required', 'numeric', 'exists:buku,id_buku'],
+            'id_user' => ['sometimes', 'required', 'numeric', 'exists:users,id_user'],
+            'jumlah_pinjaman' => ['sometimes', 'required', 'integer', 'min:1'],
+            'tgl_pinjaman' => ['sometimes', 'required', 'date'],
+            'tgl_pengembalian' => ['sometimes', 'required', 'date'],
+        ]);
+
+        $peminjaman = Peminjaman::where('id_peminjaman', $id)->firstOrFail();
+
+        if ($data['id_buku'] == $peminjaman->id_buku && (($peminjaman->buku->stok + $peminjaman->jumlah_pinjaman) < $data['jumlah_pinjaman'])) {
+            return redirect()->route('pengembalian.index')->with(['status' => 'ERROR', 'message' => 'Stok buku tidak mencukupi']);
+        }
+
+        if ($data['id_buku'] == $peminjaman->id_buku) {
+            $difference = $peminjaman->jumlah_pinjaman - $data['jumlah_pinjaman'];
+            $peminjaman->buku->stok += $difference;
+
+            $peminjaman->buku->save();
+        }
+
+        $oldIdBuku = $peminjaman->id_buku;
+        $oldJumlahPinjaman = $peminjaman->jumlah_pinjaman;
+
+        $peminjaman->fill($data);
+        $peminjaman->save();
+        $peminjaman->load('buku', 'user');
+
+        if ($oldIdBuku != $peminjaman->id_buku) {
+            $oldBukuData = Buku::where('id_buku', $oldIdBuku)->firstOrFail();
+
+            $oldBukuData->stok += $oldJumlahPinjaman;
+            $oldBukuData->save();
+
+            $peminjaman->buku->stok -= $peminjaman->jumlah_pinjaman;
+            $peminjaman->buku->save();
+        }
+
         return redirect()->route('peminjaman.index')->with('status', "SUCCESS");
     }
 
     public function destroy($id): RedirectResponse
     {
-        Peminjaman::destroy($id);
+        $peminjaman = Peminjaman::find($id)->first();
+
+        $peminjaman->buku->stok += $peminjaman->jml_pinjam;
+        $peminjaman->buku->save();
+
+        $peminjaman->delete();
+
         return redirect()->route('peminjaman.index')->with('status', "SUCCESS");
     }
 }
