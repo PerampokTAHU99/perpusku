21a08652a8f4fe02e7bda90874a8dbab51920724
Adrian Maulani <maulani.ad@gmail.com>
2024-06-26 15:34:52 +0700

    feat(peminjaman): add model relations

--- app/Models/Peminjaman.php
+++ app/Models/Peminjaman.php
@@ -4,11 +4,30 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Peminjaman extends Model
 {
     use HasFactory;
 
     protected $table = 'peminjaman';
+    protected $primaryKey = 'id_peminjaman';
 
+    protected $fillable = [
+        'id_buku',
+        'id_user',
+        'tgl_pinjaman',
+        'tgl_pengembalian',
+        'jumlah_pinjaman'
+    ];
+
+    public function buku(): BelongsTo
+    {
+        return $this->belongsTo(Buku::class, 'id_buku', 'id_buku');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id_user');
+    }
 }
