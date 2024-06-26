76253c187772a3e76aee931c2e19f276cde24712
Adrian Maulani <maulani.ad@gmail.com>
2024-06-26 10:01:26 +0700

    feat(buku): add buku routers

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+use App\Http\Controllers\BukuController;
 
 Route::get('/', function () {
     return view('dashboard');
 });
 
-Route::get('/buku', function () {
-    return view('buku');
-});
+Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+
+Route::post('/buku', [BukuController::class, 'create'])->name('buku.create');
+
+Route::post('/buku/{id}', [BukuController::class, 'update'])->name('buku.update');
+
+Route::post('/buku/{id}/destroy', [BukuController::class, 'destroy'])->name('buku.destroy');
