b35a3ab59d443ff7e7f339ce95a9702e02a26e52
Adrian Maulani <maulani.ad@gmail.com>
2024-06-26 23:55:16 +0700

    feat(pengembalian): add model relations

--- app/Models/Pengembalian.php
+++ app/Models/Pengembalian.php
@@ -9,4 +9,27 @@ class Pengembalian extends Model
 {
     use HasFactory;
     protected $table = 'pengembalian';
+    protected $primaryKey = 'id_peminjaman';
+
+    protected $fillable = [
+        'id_peminjaman',
+        'id_buku',
+        'id_user',
+        'tgl_pengembalian',
+    ];
+
+    public function peminjaman()
+    {
+        return $this->belongsTo(Peminjaman::class, 'id_peminjaman', 'id_peminjaman');
+    }
+
+    public function buku()
+    {
+        return $this->belongsTo(Buku::class, 'id_buku', 'id_buku');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id_user', 'id_user');
+    }
 }
