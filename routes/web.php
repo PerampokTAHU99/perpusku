607c2797d16c255cd290aca1163070aedcbd22f0
Adrian Maulani <maulani.ad@gmail.com>
2024-06-26 10:12:52 +0700

    KONFLIK COK

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::get('/anggota', function () {
     ]);
 });
 
-Route::get('/buku', function () {
-    return view('buku');
-});
+Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+
+Route::post('/buku', [BukuController::class, 'create'])->name('buku.create');
+
+Route::post('/buku/{id}', [BukuController::class, 'update'])->name('buku.update');
+
+Route::post('/buku/{id}/destroy', [BukuController::class, 'destroy'])->name('buku.destroy');
