a5cfff5977a777cff2049b19d68cce141e9c070b
Adrian Maulani <maulani.ad@gmail.com>
2024-06-27 09:00:56 +0700

    feat(denda): add model relations

--- app/Models/Denda.php
+++ app/Models/Denda.php
@@ -4,9 +4,44 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Denda extends Model
 {
     use HasFactory;
     protected $table = 'denda';
+
+    protected $primaryKey = 'id_denda';
+
+    protected $fillable = [
+        'id_peminjaman',
+        'id_pengembalian',
+        'id_buku',
+        'id_user',
+        'keterangan',
+        'lama_denda',
+        'nominal',
+        'tgl_denda',
+        'is_lunas'
+    ];
+
+    public function peminjaman(): BelongsTo
+    {
+        return $this->belongsTo(Peminjaman::class, 'id_peminjaman');
+    }
+
+    public function pengembalian(): BelongsTo
+    {
+        return $this->belongsTo(Pengembalian::class, 'id_pengembalian');
+    }
+
+    public function buku(): BelongsTo
+    {
+        return $this->belongsTo(Buku::class, 'id_buku');
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'id_user');
+    }
 }
