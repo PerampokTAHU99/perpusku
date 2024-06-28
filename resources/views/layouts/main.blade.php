e7d0fa6a4f15c485ff94ca9479d300e105edae1e
Adrian Maulani <maulani.ad@gmail.com>
2024-06-28 14:26:57 +0700

    feat(auth): redirect during auth

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -152,13 +152,13 @@
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
+                        <span aria-hidden="true">x</span>
                     </button>
                 </div>
                 <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="\login">Logout</a>
+                    <a class="btn btn-primary" href="{{ route('auth.logout') }}">Logout</a>
                 </div>
             </div>
         </div>
